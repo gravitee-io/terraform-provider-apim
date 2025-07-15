cb92e8375b24f9d0a758a8deb067811a80af3057
Benoit Bordigoni <benoit.bordigoni@graviteesource.com>
2025-07-15 18:31:28 +0200

    fix: fifth generation

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ terraform {
   required_providers {
     apim = {
       source  = "gravitee-io/apim"
-      version = "0.4.0"
+      version = "0.4.1"
     }
   }
 }
--- internal/sdk/graviteeapim.go
+++ internal/sdk/graviteeapim.go
@@ -144,9 +144,9 @@ func WithTimeout(timeout time.Duration) SDKOption {
 // New creates a new instance of the SDK with the provided options
 func New(opts ...SDKOption) *GraviteeApim {
 	sdk := &GraviteeApim{
-		SDKVersion: "0.4.0",
+		SDKVersion: "0.4.1",
 		sdkConfiguration: config.SDKConfiguration{
-			UserAgent:  "speakeasy-sdk/terraform 0.4.0 2.656.9 0.2.0 github.com/gravitee-io/terraform-provider-apim/internal/sdk",
+			UserAgent:  "speakeasy-sdk/terraform 0.4.1 2.656.9 0.2.0 github.com/gravitee-io/terraform-provider-apim/internal/sdk",
 			Globals:    globals.Globals{},
 			ServerList: ServerList,
 		},
