b36e361bc5198efc218c0721e830778eed7f3166
Benoit Bordigoni <benoit.bordigoni@graviteesource.com>
2026-01-27 12:02:44 +0100

    fix: add extras to CI and tofu tests
    chore: use 4.9.x to compare OAS
    docs: update README with issues information
    tests: add open tofu tests on examples
    chore: check OAS changes
    chore: add release notification

---
+++ hack/scripts/trigger-slack-notification.mjs
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {triggerPipeline} from "./lib/circleci.mjs";
+
+import {LOG, toggleVerbosity} from "./lib/index.mjs";
+
+const VERBOSE = argv.verbose;
+const TRIGGER = argv["trigger"];
+const BRANCH = argv["branch"];
+const JOB_NAME = argv["job-name"];
+const JOB_STATUS = argv["job-status"];
+const JOB_URL = argv["job-url"];
+const RELEASE_VERSION = argv["release-version"];
+
+toggleVerbosity(VERBOSE);
+
+LOG.blue(`Triggering pipeline`);
+
+const parameters = {
+    trigger: TRIGGER,
+    job_name: JOB_NAME,
+    job_status: JOB_STATUS,
+    job_url: JOB_URL,
+    release_version: RELEASE_VERSION
+};
+
+LOG.blue(`
+  Branch: ${BRANCH}
+  Parameters: ${JSON.stringify(parameters)},
+`);
+
+const pipelineURL = await triggerPipeline(parameters, BRANCH);
+
+LOG.blue(`Pipeline is running at ${pipelineURL}`);
