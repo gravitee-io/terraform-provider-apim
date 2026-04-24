0877efab2f5fc10204786f41932c06d7efb6279b
Benoit Bordigoni <benoit.bordigoni@graviteesource.com>
2026-04-24 13:23:28 +0200

    feat: custom API key field

---
+++ examples/use-cases/subscription-api_app_subscription_apikey/api-app-subscription.tf
@@ -0,0 +1,78 @@
+resource "apim_apiv4" "simple-api-subscribed-apikey" {
+  # should match the resource name
+  hrid            = "simple-api-subscribed-apikey"
+  name            = "[Terraform] Simple PROXY API With API KEY Plan"
+  description     = "A simple API with API KEY meant to subscribed using Terraform"
+  version         = "1.0"
+  type            = "PROXY"
+  state           = "STARTED"
+  visibility      = "PUBLIC"
+  lifecycle_state = "PUBLISHED"
+  listeners = [
+    {
+      http = {
+        entrypoints = [
+          {
+            type = "http-proxy"
+          }
+        ]
+        paths = [
+          {
+            path = "/simple-api-subscribed-apikey"
+          }
+        ]
+        type = "HTTP"
+      }
+    }
+  ]
+  endpoint_groups = [
+    {
+      name = "Default HTTP proxy group"
+      type = "http-proxy"
+      endpoints = [
+        {
+          name = "Default HTTP proxy"
+          type = "http-proxy"
+          configuration = jsonencode({
+            target = "https://api.gravitee.io/echo"
+          })
+        }
+      ]
+      services = {}
+    }
+  ]
+  analytics = {
+    enabled = true
+  }
+  plans = [
+    {
+      hrid        = "apikey"
+      description = "API Key"
+      mode        = "STANDARD"
+      name        = "Api Key"
+      status      = "PUBLISHED"
+      type        = "API"
+      validation  = "AUTO"
+      security = {
+        type = "API_KEY"
+      }
+    }
+  ]
+}
+
+
+resource "apim_application" "simple-app-subscribed-apikey" {
+  # should match the resource name
+  hrid        = "simple-app-subscribed-apikey"
+  name        = "[Terraform] Simple Application"
+  description = "Subscription tests application"
+}
+
+resource "apim_subscription" "simple-subscription-apikey" {
+  # should match the resource name
+  hrid             = "simple-subscription-apikey"
+  api_hrid         = apim_apiv4.simple-api-subscribed-apikey.hrid
+  plan_hrid        = apim_apiv4.simple-api-subscribed-apikey.plans[0].hrid
+  application_hrid = apim_application.simple-app-subscribed-apikey.hrid
+  custom_api_key   = "custom-api-key-123456789"
+}
